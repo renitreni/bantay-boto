10914612357ed7d06c2e8a9a61516fa4a3587462
rtrenuela <rtrenuela@objectbright.com>
2021-12-24 09:36:08 +0800

    update

--- resources/views/livewire/blog-list.blade.php
+++ resources/views/livewire/blog-list.blade.php
@@ -26,7 +26,7 @@
                                         <h2>{{ $article->title }}</h2>
                                     </a>
                                     <div>
-                                        {!! \Illuminate\Support\Str::words($article->content, 15) !!}
+                                        {!! \Illuminate\Support\Str::limit($article->content, 50) !!}
                                     </div>
                                     <ul class="blog-info-link">
                                         <li>
