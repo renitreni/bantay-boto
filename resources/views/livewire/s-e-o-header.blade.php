f5582e5d67ae79c4791a212c6a5d85dbfc11d8c7
rtrenuela <rtrenuela@objectbright.com>
2021-12-27 09:58:28 +0800

    update

--- resources/views/livewire/s-e-o-header.blade.php
+++ resources/views/livewire/s-e-o-header.blade.php
@@ -1,13 +1,20 @@
 <head>
-<title>
-    {{ $title }} | {{ config('app.name', 'Laravel') }}
-</title>
-<meta name="description" content="{{ $content }}">
-<meta name="keywords" content="{{ $tags }}">
-<link rel=”canonical” href=”{{ route('home') }}”/>
-<link rel="alternate" href="{{ route('home') }}" hreflang="en-en"/>
-<link rel="alternate" href="{{ route('home') }}" hreflang="de-de"/>
-<link rel="alternate" href="{{ route('home') }}" hreflang="fr-fr"/>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name=”robots” content=”noindex, follow”/>
+    <title>
+        {{ $title }} | {{ config('app.name', 'Laravel') }}
+    </title>
+    <meta name="description" content="{{ $content }}">
+    <meta name="keywords" content="{{ $tags }}">
+    <link rel=”canonical” href=”{{ route('home') }}”/>
+    <link rel="alternate" href="{{ route('home') }}" hreflang="en-en"/>
+    <link rel="alternate" href="{{ route('home') }}" hreflang="de-de"/>
+    <link rel="alternate" href="{{ route('home') }}" hreflang="fr-fr"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name=”robots” content=”noindex, follow”/>
+
+    <meta property="og:url" content="{{ url()->current()  }}"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="{{ $title }}"/>
+    <meta property="og:description" content="{{ $content }}"/>
+    <meta property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"/>
 </head>
