dbd7812c0e016d45fbe33a9806908ba0c9741366
Mohammed <Aziz.Mohammed@Aziz-Mohammed.local>
2022-01-13 07:18:53 +0800

    hadji:update

--- resources/views/livewire/confim-vote.blade.php
+++ resources/views/livewire/confim-vote.blade.php
@@ -1,5 +1,16 @@
 <div>
-    <div class="my-4">
+    {{-- <div class="my-4">
         <h1>Thank you, your vote has been counted!</h1>
+    </div> --}}
+    <div class="card">
+        <div class="card-body">
+            <div class="container">        
+                <div class="my-5 row justify-content-center">
+                    <p class="card-text text-uppercase font-weight-bolder" style="font-size: 35px; font-weight: bolder;">
+                    Thank you, your vote has been counted!
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
