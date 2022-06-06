22243fdba11a1c38b93a9c9c157a9ced6181ee38
rtrenuela <rtrenuela@objectbright.com>
2022-06-06 10:42:07 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,21 @@
 
             gtag('config', 'G-1B8VPMXEZJ');
         </script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-56PLK8F');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body class="font-sans antialiased bg-light">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56PLK8F"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         <x-jet-banner />
         @livewire('navigation-menu')
 
